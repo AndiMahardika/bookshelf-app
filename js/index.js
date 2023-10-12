39421152551cf87fa684c5522d6a9c687c6c0fe7
AndiMahardika <andi.mahardika.mulya@gmail.com>
2023-10-11 21:57:28 -0700

    modified index.html & js/index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 const books = [];
 const RENDER_EVENT = `render_book`;
 
-document.addEventListener(`DOMContentLoaded`,function(){
-  const inputBook = document.getElementById(`inputBook`);
+document.addEventListener(`DOMContentLoaded`, function(){  
+  const submitBook = document.getElementById(`inputBook`);
 
-  inputBook.addEventListener(`submit`, function(event){
-    event.preventDefault();
+  submitBook.addEventListener(`submit`,function(event){
+    event.preventDefault()
     addBook();
-  });
-});
+  })
+})
 
 function addBook(){
   const bookTitle = document.getElementById(`inputBookTitle`).value;
@@ -36,10 +36,92 @@ function generateBookObject(id, title, author, year, status){
     status
   }
 }
+ 
+function itemUncompleteBookshelfList(bookObject){
+  const bookGroupUncompleted = document.createElement(`ul`);
+  bookGroupUncompleted.classList.add(`list-group` ,`mb-2`);
+  bookGroupUncompleted.innerHTML = `<li class="list-group-item active" aria-current="true" style="background-color: #3294cd;"><strong>${bookObject.title}</strong></li>
+                                    <li class="list-group-item"><span class="fw-semibold">Penulis</span> : ${bookObject.author}</li>
+                                    <li class="list-group-item"><span class="fw-semibold">Tahun</span> : ${bookObject.year}</li>
+                                    <li class="list-group-item text-evenly">
+                                      <button class="btn mx-1 border border-success" style="background-color: #94cd32; --bs-border-opacity: .10;" type="button"><i class="fa-solid fa-check fa-lg btn-complete" style="color: #fff;" data-bookid="${bookObject.id}"></i></button>
+                                      <button class="btn btn-primary mx-1" type="button"><i class="fa-solid fa-pen-to-square btn-edit" data-bookid="${bookObject.id}"></i></button>
+                                      <button class="btn btn-danger mx-1" type="button"><i class="fa-solid fa-trash-can btn-delete" data-bookid="${bookObject.id}"></i></button>
+                                    </li>`
+
+  return bookGroupUncompleted;                      
+}
+
+function itemCompleteBookshelfList(bookObject){
+  const bookGroupCompleted = document.createElement(`ul`)
+  bookGroupCompleted.classList.add(`list-group` ,`mb-2`);
+  bookGroupCompleted.innerHTML = `<li class="list-group-item active" aria-current="true" style="background-color: #3294cd;"><strong>${bookObject.title}</strong></li>
+                                  <li class="list-group-item"><span class="fw-semibold">Penulis</span> : ${bookObject.author}</li>
+                                  <li class="list-group-item"><span class="fw-semibold">Tahun</span> : ${bookObject.year}</li>
+                                  <li class="list-group-item text-evenly">
+                                    <button class="btn mx-1 border border-success" style="background-color: #ff8c00; --bs-border-opacity: .10;"  type="button"><i class="fa-solid fa-rotate-right fa-flip-horizontal fa-lg btn-undo" style="color: #ffffff;" data-bookid="${bookObject.id}"></i></button>
+                                    <button class="btn btn-primary mx-1" type="button"><i class="fa-solid fa-pen-to-square btn-edit" data-bookid="${bookObject.id}"></i></button>
+                                    <button class="btn btn-danger mx-1" type="button"><i class="fa-solid fa-trash-can btn-delete" data-bookid="${bookObject.id}"></i></button>
+                                  </li>`
+
+  return bookGroupCompleted;                                  
+}
+
+function findBook(bookId){
+  for (const bookTarget of books) {
+    if (bookTarget.id == bookId) {
+      return bookTarget;
+    } 
+  }
+  return null;
+}
+
+function addBookToComplete(bookId){
+  const bookTarget = findBook(bookId);
+
+  bookTarget.status = true;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function undoBookToComplete(bookId){
+  const bookTarget = findBook(bookId);
+
+  bookTarget.status = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+document.addEventListener(`click`, function(event){
+  if(event.target.classList.contains(`btn-complete`)){
+    const bookid = event.target.dataset.bookid;
+    addBookToComplete(bookid);
+  }
+  if(event.target.classList.contains(`btn-undo`)){
+    const bookid = event.target.dataset.bookid;
+    undoBookToComplete(bookid);
+  }
+  if(event.target.classList.contains(`btn-edit`)){
+    const bookid = event.target.dataset.bookid;
+    console.log(bookid);
+  }
+  if(event.target.classList.contains(`btn-delete`)){
+    const bookid = event.target.dataset.bookid;
+    console.log(bookid);
+  }
+})
 
 // RENDER_EVENT
 document.addEventListener(RENDER_EVENT, function(){
-  for (const item of books) {
-    console.log(item);
+  const uncompletedBOOKList = document.getElementById(`incompleteBookshelfList`);
+  uncompletedBOOKList.innerHTML = ``;
+
+  const completedBOOKList = document.getElementById(`completeBookshelfList`);
+  completedBOOKList.innerHTML = ``;
+
+  for (const bookItem of books) {
+    if(!bookItem.status){
+      uncompletedBOOKList.appendChild((itemUncompleteBookshelfList(bookItem)));
+    } else {
+      completedBOOKList.appendChild((itemCompleteBookshelfList(bookItem)));
+    }
   }
 })
